ac44e46de0282aeb9652dfc9df99b12e02504298
NextFire <git@yuru.moe>
2024-09-10 20:35:40 +0200

    [calendar] assert_single select user calendar

--- nanapi/database/calendar/user_calendar_select.edgeql
+++ nanapi/database/calendar/user_calendar_select.edgeql
@@ -1,4 +1,4 @@
 with
-  discord_id := <int64>$discord_id
-select assert_single(calendar::UserCalendar) { ** }
-filter .user.discord_id = discord_id
+  discord_id := <int64>$discord_id,
+  calendar := (select calendar::UserCalendar filter .user.discord_id = discord_id),
+select assert_single(calendar) { ** }
--- nanapi/database/calendar/user_calendar_select.py
+++ nanapi/database/calendar/user_calendar_select.py
@@ -5,9 +5,9 @@ from pydantic import BaseModel, TypeAdapter
 
 EDGEQL_QUERY = r"""
 with
-  discord_id := <int64>$discord_id
-select assert_single(calendar::UserCalendar) { ** }
-filter .user.discord_id = discord_id
+  discord_id := <int64>$discord_id,
+  calendar := (select calendar::UserCalendar filter .user.discord_id = discord_id),
+select assert_single(calendar) { ** }
 """
 
 
