ce3377cbddeead395326c17374586bea83252e72
NextFire <git@yuru.moe>
2024-11-09 18:40:14 +0100

    local_settings: cleanup

--- nanapi/example.local_settings.py
+++ nanapi/example.local_settings.py
@@ -2,41 +2,45 @@
 
 # from zoneinfo import ZoneInfo
 
-# INSTANCE_NAME = 'nanapi'
 # LOG_LEVEL = 'INFO'
-# ERROR_WEBHOOK_URL = ''
+# ERROR_WEBHOOK_URL = None
 # PROFILING = False
 
-# FastAPI/Uvicorn
+## General
+# INSTANCE_NAME = 'nanapi'
+# TZ = ZoneInfo('Europe/Paris')
+
+## EdgeDB
+# EDGEDB_CONFIG = dict()
+
+## Meilisearch
+# MEILISEARCH_HOST_URL = 'http://localhost:7700'
+# MEILISEARCH_CONFIG = dict()
+
+## FastAPI/Uvicorn
 # FASTAPI_APP = 'nanapi.fastapi:app'
 # FASTAPI_CONFIG = dict()
 # HYPERCORN_CONFIG = dict(workers=4, accesslog='-')
-# Security
+
+## Security
 BASIC_AUTH_USERNAME = 'username'
 BASIC_AUTH_PASSWORD = 'password'
 # JWT_ALGORITHM = 'HS256'
 # JWT_EXPIRE_MINUTES = 30
 JWT_SECRET_KEY = ''  # openssl rand -hex 32
-# OpenAPI
+
+## OpenAPI
 # OPENAPI_URL = '/openapi.json'
 # DOCS_URL = '/docs'
 # SWAGGER_UI_OAUTH2_REDIRECT_URL = DOCS_URL + '/oauth2-redirect'
 # REDOC_URL = '/redoc'
 
-# EdgeDB
-# EDGEDB_CONFIG = dict()
-
-# Meilisearch
-# MEILISEARCH_HOST_URL = 'http://localhost:7700'
-# MEILISEARCH_CONFIG = dict()
-
-# General
-# TZ = ZoneInfo('Europe/Paris')
-# PRODUCER_UPLOAD_ENDPOINT = 'https://producer.japan7.bde.enseeiht.fr'
-# PRODUCER_TOKEN = ''
-
-# AniList
+## AniList
 # LOW_PRIORITY_THRESH = 30
 
-# MyAnimeList
+## MyAnimeList
 MAL_CLIENT_ID = ''
+
+## Producer
+# PRODUCER_UPLOAD_ENDPOINT = 'https://producer.japan7.bde.enseeiht.fr'
+# PRODUCER_TOKEN = ''
--- nanapi/settings.py
+++ nanapi/settings.py
@@ -1,38 +1,49 @@
 from typing import Any
 from zoneinfo import ZoneInfo
 
-INSTANCE_NAME = 'nanapi'
 LOG_LEVEL = 'INFO'
 ERROR_WEBHOOK_URL = None
 PROFILING = False
 
-# FastAPI/Uvicorn
+## General
+INSTANCE_NAME = 'nanapi'
+TZ = ZoneInfo('Europe/Paris')
+
+## EdgeDB
+EDGEDB_CONFIG: dict[str, Any] = dict()
+
+## Meilisearch
+MEILISEARCH_HOST_URL = 'http://localhost:7700'
+MEILISEARCH_CONFIG: dict[str, Any] = dict()
+
+## FastAPI/Uvicorn
 FASTAPI_APP = 'nanapi.fastapi:app'
 FASTAPI_CONFIG: dict[str, Any] = dict()
 HYPERCORN_CONFIG: dict[str, Any] = dict(workers=4, accesslog='-')
-# Security
+
+## Security
+# BASIC_AUTH_USERNAME = 'username'
+# BASIC_AUTH_PASSWORD = 'password'
 JWT_ALGORITHM = 'HS256'
 JWT_EXPIRE_MINUTES = 30
-# OpenAPI
+# JWT_SECRET_KEY = ''  # openssl rand -hex 32
+
+## OpenAPI
 OPENAPI_URL = '/openapi.json'
 DOCS_URL = '/docs'
 SWAGGER_UI_OAUTH2_REDIRECT_URL = DOCS_URL + '/oauth2-redirect'
 REDOC_URL = '/redoc'
 
-# EdgeDB
-EDGEDB_CONFIG: dict[str, Any] = dict()
+## AniList
+LOW_PRIORITY_THRESH = 30
 
-# Meilisearch
-MEILISEARCH_HOST_URL = 'http://localhost:7700'
-MEILISEARCH_CONFIG: dict[str, Any] = dict()
+## MyAnimeList
+# MAL_CLIENT_ID = ''
 
-# General
-TZ = ZoneInfo('Europe/Paris')
+## Producer
 PRODUCER_UPLOAD_ENDPOINT = 'https://producer.japan7.bde.enseeiht.fr'
 PRODUCER_TOKEN = ''
 
-# AniList
-LOW_PRIORITY_THRESH = 30
 
 try:
     from .local_settings import *  # noqa: F403
