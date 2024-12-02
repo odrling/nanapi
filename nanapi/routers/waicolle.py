86873816b3b43090effb3302e06c0bc4a22b867d
odrling <florianbadie@odrling.xyz>
2024-12-02 20:42:08 +0100

    [waicolle] don’t commit already completed trades

--- nanapi/routers/waicolle.py
+++ nanapi/routers/waicolle.py
@@ -1170,6 +1170,8 @@ async def commit_trade(id: UUID,
             trade = await trade_get_by_id(tx, id=id)
             if trade is None:
                 raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+            if trade.completed_at is not None:
+                raise HTTPException(status_code=status.HTTP_409_CONFLICT)
             commit = await trade_commit(tx, id=id)
             assert commit is not None
 
